48365b03aafbca550d876daf1ee21774f5cbdbd5
hansen.wu <hansen.wu@pinhome.id>
2021-04-02 18:25:34 +0700

    add htaccess

--- src/static/skill-data.ts
+++ src/static/skill-data.ts
@@ -4,67 +4,67 @@ export default ref({
   language: [
       {
         name: "Javascript",
-        url: "/src/assets/images/skill/js.png"
+        url: "/skill/js.png"
       },    
       {
         name: "Typescript",
-        url: "/src/assets/images/skill/ts.png"
+        url: "/skill/ts.png"
       },
       {
         name: "PHP",
-        url: "/src/assets/images/skill/php.png"
+        url: "/skill/php.png"
       },
       {
         name: "Dart",
-        url: "/src/assets/images/skill/dart.png"
+        url: "/skill/dart.png"
       },
       {
         name: "Go",
-        url: "/src/assets/images/skill/go.png"
+        url: "/skill/go.png"
       },
   ],
   framework: [
     {
       name: "Flutter",
-      url: "/src/assets/images/skill/flutter.png"
+      url: "/skill/flutter.png"
     },
     {
       name: "Vue JS",
-      url: "/src/assets/images/skill/vue.png"
+      url: "/skill/vue.png"
     }, 
     {
       name: "Laravel",
-      url: "/src/assets/images/skill/laravel.png"
+      url: "/skill/laravel.png"
     }, 
     {
       name: "Phalcon",
-      url: "/src/assets/images/skill/phalcon.png"
+      url: "/skill/phalcon.png"
     }, 
   ],
   others: [
     {
       name: "HTML",
-      url: "/src/assets/images/skill/html.png"
+      url: "/skill/html.png"
     },
     {
       name: "CSS",
-      url: "/src/assets/images/skill/css.png"
+      url: "/skill/css.png"
     },
     {
       name: "SASS",
-      url: "/src/assets/images/skill/sass.png"
+      url: "/skill/sass.png"
     },
     {
       name: "Stylus",
-      url: "/src/assets/images/skill/stylus.png"
+      url: "/skill/stylus.png"
     },
     {
       name: "Jquery",
-      url: "/src/assets/images/skill/jquery.png"
+      url: "/skill/jquery.png"
     },
     {
       name: "Git",
-      url: "/src/assets/images/skill/git.png"
+      url: "/skill/git.png"
     },
   ]
 });
