d01d927ef524412fe1b7de0bd111c39449031a3a
hansen.wu <hansen.wu@pinhome.id>
2021-04-03 23:29:52 +0700

    fix portofolio data

--- src/static/portofolio-data.ts
+++ src/static/portofolio-data.ts
@@ -1,22 +1,73 @@
 import { ref } from "vue";
 
 export default ref([
+
     {
-        name: "KMBD",
-        src: "/portofolio/kmbd.jpg",
+        name: "Pattidana CDM",
+        src: "/portofolio/pattidana.jpg",
+        showInHomePage: true,
         description: `
             <p>
             Feature: <br>
-            - Organization Profile <br>
-            - Admin Panel/ Content Management System <br>
-            - Member attendance system by using RFID Card <br>
+            - Admin Panel <br>
+            - Cashier Panel <br>
+            - Event information & register ancestor <br>
             <br>
             Tech Stack: <br>
             - Laravel <br>
             - MySql <br>
+            - VueJs <br>
             <br>
-            You can access the Organization Profile sites
-            <a href="https://kmbd.info" target="_blank" style="color: white">here</a>
+            You can access the Pattidana Event sites  
+            <a href="https://pattidana.dhammamanggala.org" target="_blank" style="color: white">here</a>
+            </p>
+        `
+    },
+    {
+        name: "SDATR Pasuruan",
+        src: "/portofolio/sdatr-pasuruan.jpg",
+        showInHomePage: true,
+        description: `
+            <p>
+            Feature: <br>
+            - Admin Panel/ Content Management System <br>
+            - SDATR profile & information <br>
+            - Submission for “Peil Banjir” and “Rekom Tanah pengairan” <br>
+            <br>
+            Tech Stack: <br>
+            - Laravel <br>
+            - MySql <br>
+            </p>
+        `
+    },
+    {
+        name: "Landing Page",
+        src: "/portofolio/landing-page.jpg",
+        description: `
+            <p>
+            Tech Stack: <br>
+            VueJs <br>
+            <br>
+            You can access the sites 
+            <a href="https://landingpage.hansengotama.com" target="_blank" style="color: white">here</a>
+            </p>
+        `
+    },
+    {
+        name: "Hotel Bintang Mulia",
+        src: "/portofolio/bintang-mulia.jpg",
+        showInHomePage: true,
+        description: `
+            <p>
+            Feature: <br>
+            - Hotel profile and information <br>
+            <br>
+            Tech Stack: <br>
+            - Laravel <br>
+            - VueJs <br>
+            <br>
+            You can access the Hotel Profile sites
+            <a href="https://hotelbintangmulia.net" target="_blank" style="color: white">here</a>
             </p>
         `
     },
@@ -43,71 +94,21 @@ export default ref([
         `
     },
     {
-        name: "Hotel Bintang Mulia",
-        src: "/portofolio/bintang-mulia.jpg",
-        showInHomePage: true,
+        name: "KMBD",
+        src: "/portofolio/kmbd.jpg",
         description: `
             <p>
             Feature: <br>
-            - Hotel profile and information <br>
-            <br>
-            Tech Stack: <br>
-            - Laravel <br>
-            - VueJs <br>
-            <br>
-            You can access the Hotel Profile sites
-            <a href="https://hotelbintangmulia.net" target="_blank" style="color: white">here</a>
-            </p>
-        `
-    },
-    {
-        name: "Landing Page",
-        src: "/portofolio/landing-page.jpg",
-        description: `
-            <p>
-            Tech Stack: <br>
-            VueJs <br>
-            <br>
-            You can access the sites 
-            <a href="https://landingpage.hansengotama.com" target="_blank" style="color: white">here</a>
-            </p>
-        `
-    },
-    {
-        name: "SDATR Pasuruan",
-        src: "/portofolio/sdatr-pasuruan.jpg",
-        showInHomePage: true,
-        description: `
-            <p>
-            Feature: <br>
+            - Organization Profile <br>
             - Admin Panel/ Content Management System <br>
-            - SDATR profile & information <br>
-            - Submission for “Peil Banjir” and “Rekom Tanah pengairan” <br>
+            - Member attendance system by using RFID Card <br>
             <br>
             Tech Stack: <br>
             - Laravel <br>
             - MySql <br>
-            </p>
-        `
-    },
-    {
-        name: "Pattidana CDM",
-        src: "/portofolio/pattidana.jpg",
-        showInHomePage: true,
-        description: `
-            <p>
-            Feature: <br>
-            - Admin Panel <br>
-            - Cashier Panel <br>
-            - Event information & register ancestor <br>
             <br>
-            Tech Stack: <br>
-            - Laravel <br>
-            - MySql <br>
-            - VueJs <br>
-            <br>
-            You can access the Pattidana Event sites  
-            <a href="https://pattidana.dhammamanggala.org" target="_blank" style="color: white">here</a>
+            You can access the Organization Profile sites
+            <a href="https://kmbd.info" target="_blank" style="color: white">here</a>
             </p>
         `
     },
